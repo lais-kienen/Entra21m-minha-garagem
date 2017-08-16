ba32ef50d1237685ba018a666f10d3b13675544e
Lais Kienen <lais_kienen@hotmail.com>
2017-08-16 11:05:44 -0300

    Desenvolvimento dos botões alterar e salvar do cadastro carro

--- src/view/JFrameCadastroCarro.java
+++ src/view/JFrameCadastroCarro.java
@@ -5,7 +5,6 @@
  */
 package view;
 
-import Utilitarios2;
 import dao.CarroDAO;
 import database.Utilitarios;
 import java.sql.Date;
@@ -189,8 +188,7 @@ public class JFrameCadastroCarro extends javax.swing.JFrame {
         jComboBoxAnoLancamento.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1920", "1921", "1922", "1923", "1924", "1925", "1926", "1927", "1928", "1929", "1930", "1931", "1932", "1933", "1934", "1935", "1936", "1937", "1938", "1939", "1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947", "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017" }));
         jComboBoxAnoLancamento.setSelectedIndex(97);
 
-        jComboBoxTipoPneu.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "205/55R16", "215/45R17", "225/55R17", "225/45R17", "225/40R18", "225/40R18", "225/40R18", "245/35R20", "285/35R20", "245/35R20", "285/35R20" }));
-        jComboBoxTipoPneu.setSelectedIndex(-1);
+        jComboBoxTipoPneu.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "15", "16", "17", "18", "20", " " }));
 
         try {
             jFormattedTextFieldPlaca.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("UUU-####")));
@@ -421,7 +419,7 @@ public class JFrameCadastroCarro extends javax.swing.JFrame {
 
         meuCarro.setTipoPneu(Short.parseShort(jComboBoxTipoPneu.getSelectedItem().toString()));
 
-        meuCarro.setQuilometragem(Float.parseFloat(jFormattedTextFieldQuilometragem.getText()));
+        meuCarro.setQuilometragem(Float.parseFloat(jFormattedTextFieldQuilometragem.getText().toString().replace("Km", "")));
 
         meuCarro.setPlaca(jFormattedTextFieldPlaca.getText());
 
@@ -434,15 +432,25 @@ public class JFrameCadastroCarro extends javax.swing.JFrame {
         meuCarro.setEstaFuncional(jRadioButtonEstaFuncionalSim.isSelected());
 
         CarroDAO dao = new CarroDAO();
-        int codigo = dao.inserir(meuCarro);
-        if (codigo != Utilitarios.NAO_FOI_POSSIVEL_INSERIR) {
-            JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso");
+        if (!jLabelID.getText().equals("")) {
+            meuCarro.setId(Integer.parseInt(jLabelID.getText()));
 
+            int codigo = dao.alterar(meuCarro);
+            if (codigo != Utilitarios.NAO_FOI_POSSIVEL_ALTERAR) {
+                JOptionPane.showMessageDialog(null, "Alteração realizada com sucesso!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Não foi possível alterar.");
+            }
         } else {
-            JOptionPane.showMessageDialog(null, "Não foi possível inserir");
 
-            meuCarro.setId(codigo);
-            jTextFieldID.setText(String.valueOf(codigo));
+            int codigo = dao.inserir(meuCarro);
+            if (codigo != Utilitarios.NAO_FOI_POSSIVEL_INSERIR) {
+                JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso");
+                jTextFieldID.setText(String.valueOf(codigo));
+            } else {
+                JOptionPane.showMessageDialog(null, "Não foi possível inserir");
+
+            }
         }
 
     }//GEN-LAST:event_jButtonSalvarActionPerformed
